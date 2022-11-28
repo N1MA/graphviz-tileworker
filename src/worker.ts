cb03dd872d02b0f3d8f5ff1156c3c8654662667d
Nima <nima.hakimi95@gmail.com>
2022-11-28 03:38:20 +0330

    added password to env

--- src/worker.ts
+++ src/worker.ts
@@ -8,6 +8,7 @@ dotenv.config();
 const connection = {
   host: env.REDIS_HOST!,
   port: Number(env.REDIS_PORT),
+  password: env.REDIS_PASSWORD
 };
 
 const worker = new Worker('graph-viz', processor, { connection });
